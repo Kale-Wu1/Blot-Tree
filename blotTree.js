47364d6960a5c31460b4cf087217c261fb3eb3b5
Kale-Wu1 <kwu3@ocdsb.ca>
2024-08-16 01:40:06 -0400

    Hub Shape debugging (not fixed...)

--- blotTree.js
+++ blotTree.js
@@ -1,52 +1,48 @@
-// welcome to blot!
-
-// check out this guide to learn how to program in blot
-// https://blot.hackclub.com/editor?guide=start
-
 const width = 125;
 const height = 125;
 
 setDocDimensions(width, height);
 
-//User parameters
+// User parameters
 const trunkWidth = 5;
-const numBigBranches = 3;
-const numBBSections = 3; //sections per big branch
-const numSmallBranches = 2; //small branches per section
+const numBigBranches = 3; // Number of sides, for half hexagon trapezoid
+const numBBSections = 3; // sections per big branch
+const numSmallBranches = 2; // small branches per section
 
 // store final lines here
 const finalLines = [];
 
 //Trunk shape
+
 const trunk = [
-  [(width-trunkWidth)/2, 0],
-  [(width+trunkWidth)/2, 0],
-  [(2*width+trunkWidth)/4, 2*height/5],
-  [(2*width-trunkWidth)/4, 2*height/5],
-  [(width-trunkWidth)/2, 0]
+  [(width - trunkWidth) / 4, 0],
+  [(width + trunkWidth) / 2, 0],
+  [(2 * width + trunkWidth) / 4, 2 * height / 5],
+  [(2 * width - trunkWidth) / 4, 2 * height / 5],
+  [(width - trunkWidth) / 2, 0]
 ];
 finalLines.push(trunk);
 
-//Hub
-const theta = 180/numBigBranches;
-const x = width/2;
-const y = 2*height/5;
-const r = trunkWidth/2;
+var branchHub = [];
 
+//Hub shape
+const x = width / 2;
+const y = 2 * height / 5;
+const r = ((2 * width + trunkWidth) / 4 - (2 * width - trunkWidth) / 4) / 2;
+const theta = 180 / numBigBranches;
+var angle = theta;
 
-var branchHub = [
-  [(2*width+trunkWidth)/4, 2*height/5],
-  [(2*width-trunkWidth)/4, 2*height/5],
-]
-
-for(let i = 0; i < numBigBranches; i++) {
-  var xChange = r*Math.cos( (theta/2 + i*theta) /180*Math.PI);
-  var yChange = r*Math.sin( (theta*(i+1)) /180*Math.PI);
-  branchHub.push([x-xChange, y+yChange]);
+// Calculate the top vertices
+for (let i = 0; i < numBigBranches - 1; i++) {
+  angle += theta;
+  var xChange = x - r * Math.cos(theta / 180 * Math.PI);
+  var yChange = y + r * Math.sin(theta / 180 * Math.PI);
+  branchHub.push([xChange, yChange]);
 }
 
+branchHub.push(branchHub[0]);
+
 finalLines.push(branchHub);
-  
 
 // draw it
 drawLines(finalLines);
